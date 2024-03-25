a6d54867879c7d2be84cf5916da9af1c28581144
Danii9790 <162893232+Danii9790@users.noreply.github.com>
2024-03-25 12:31:43 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+//(1)Computer will generate a random Number.
+//(2)User input  a guessing number.
+//(3)Compare the one and two numbers.
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+
+    type: "number",
+    message: "please guess a number between 1-10",
+  },
+]);
+console.log(answer);
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("You won");
+} else {
+  console.log("You lost");
+}
